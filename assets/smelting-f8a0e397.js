4d6acb081dc766e42972393c02696388a335d180
KrittapornTomano <119473389+KrittapornTomano@users.noreply.github.com>
2023-06-05 23:08:54 +0700

    Add files via upload

---
+++ assets/smelting-f8a0e397.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index-55aa65e7.js";const s={},t={style:{"background-image":"url('src/assets/Dirt_background_JE2.jpg')","background-size":"cover",height:"100vh"}};function o(n,a){return c(),r("div",t)}const i=e(s,[["render",o]]);export{i as default};
