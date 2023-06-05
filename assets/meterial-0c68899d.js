3e9588a0e0c413f6e5a2e1dfd89c62fdf959fc8b
KrittapornTomano <119473389+KrittapornTomano@users.noreply.github.com>
2023-06-05 23:17:40 +0700

    Delete assets directory

--- assets/meterial-0c68899d.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as r}from"./index-55aa65e7.js";const t={},o={style:{"background-image":"url('src/assets/Dirt_background_JE2.jpg')","background-size":"cover",height:"100vh"}};function s(a,n){return c(),r("div",o)}const i=e(t,[["render",s]]);export{i as default};
