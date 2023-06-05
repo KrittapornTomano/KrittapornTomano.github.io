9d9f3a661a782749cca6e4a20240ed18e70ebcb4
KrittapornTomano <119473389+KrittapornTomano@users.noreply.github.com>
2023-06-05 21:21:51 +0700

    Add files via upload

---
+++ assets/meterial-e82cbbcf.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index-8c4f87c2.js";const t={},o={style:{"background-image":"url('src/assets/Dirt_background_JE2.jpg')","background-size":"cover",height:"100vh"}};function s(a,n){return c(),r("div",o)}const i=e(t,[["render",s]]);export{i as default};
---
+++ assets/smelting-6de1c938.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index-8c4f87c2.js";const s={},t={style:{"background-image":"url('src/assets/Dirt_background_JE2.jpg')","background-size":"cover",height:"100vh"}};function o(n,a){return c(),r("div",t)}const i=e(s,[["render",o]]);export{i as default};
